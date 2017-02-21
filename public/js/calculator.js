9222fe23844479851687451fc4b2516cf93c4bee
Matt Valdez <mattvaldez01@gmail.com>
2017-02-21 14:57:48 -0600

    condensing the code

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,138 +1,23 @@
 (function() {
 	"use strict";
 
-	///////////////////////////// TYPES IN NUMBERS /////////////////////////////////
+	/////////////////////// TYPES IN NUMBERS ///////////////////////////
 
-	function typeInNumber1(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "1";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "1";
-			}	
+	function typeInNumber() {
+		var leftDisplay = document.getElementById("input1");
+		var rightDisplay = document.getElementById("input3");
+		if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/"){
+			leftDisplay.value += this.innerText;	
+		} else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/") {
+			rightDisplay.value += this.innerText;
+		}
 	}
-	var input = document.getElementById("num1");
-	input.addEventListener("click", typeInNumber1);
-	
-	function typeInNumber2(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "2";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "2";
-			}	
-	}
-	var input = document.getElementById("num2");
-	input.addEventListener("click", typeInNumber2);
-	
-	function typeInNumber3(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "3";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "3";
-			}	
-	}
-	var input = document.getElementById("num3");
-	input.addEventListener("click", typeInNumber3);
-	
-	function typeInNumber4(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "4";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "4";
-			}	
-	}
-	var input = document.getElementById("num4");
-	input.addEventListener("click", typeInNumber4);
-	
-	function typeInNumber5(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "5";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "5";
-			}	
-	}
-	var input = document.getElementById("num5");
-	input.addEventListener("click", typeInNumber5);
-	
-	function typeInNumber6(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "6";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "6";
-			}	
-	}
-	var input = document.getElementById("num6");
-	input.addEventListener("click", typeInNumber6);
-	
-	function typeInNumber7(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "7";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "7";
-			}	
-	}
-	var input = document.getElementById("num7");
-	input.addEventListener("click", typeInNumber7);
-	
-	function typeInNumber8(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "8";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "8";
-			}	
-	}
-	var input = document.getElementById("num8");
-	input.addEventListener("click", typeInNumber8);
-	
-	function typeInNumber9(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "9";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "9";
-			}	
-	}
-	var input = document.getElementById("num9");
-	input.addEventListener("click", typeInNumber9);
-	
-	function typeInNumber0(){
-			if (input2.value !== "+" && input2.value !== "-" && input2.value !== "*" && input2.value !== "/") {
-				var inputNumber = document.getElementById("input1");
-				inputNumber.value = input1.value + "0";
-			}
-			else if (input2.value == "+" || input2.value == "-" || input2.value == "*" || input2.value == "/"){
-				var inputNumber = document.getElementById("input3");
-				inputNumber.value = input3.value + "0";
-			}	
-	}
-	var input = document.getElementById("num0");
-	input.addEventListener("click", typeInNumber0);
+	var numbers = document.getElementsByClassName("number");
 
+	for (var i = 0; i < numbers.length; i++) {
+		numbers[i].addEventListener("click", typeInNumber);
+	}
+	
 
 
 	///////////////////// TYPES IN OPERATORS /////////////////////////
@@ -198,7 +83,7 @@
 	equals.addEventListener("click", calculateMath);
 	
 
-	
+
 	///////////////////// CLEARS CALCULATOR /////////////////////////
 
 	function clearCalculator(){
