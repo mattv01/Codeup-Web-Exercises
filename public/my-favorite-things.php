6937ac4a950f6252239574a9e44f774a56d285b1
Matt Valdez <mattvaldez01@gmail.com>
2017-03-24 12:28:03 -0500

    changed background color for bootstrap's 'table-striped' class and restructured css for more readability

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -15,7 +15,24 @@ $myFavoriteThings = ["hockey", "golfing", "music", "movies", "margot robbie"];
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<!--<link rel="stylesheet" href="css/font-awesome.min.css">--> 
 	<link rel="stylesheet" type="text/css" href="">
-	<style type="text/css">table{margin:5%;font-size: 3em;}td{padding: 5%;}th{padding: 14px;}</style>
+	<style type="text/css">
+		table {
+			margin:5%;
+			font-size: 3em;
+		}
+		td {
+			padding: 5%;
+		}
+		th {
+			padding: 14px;
+		}
+		.table-striped>tbody>tr:nth-child(odd)>th {
+			background-color: honeydew;
+		}
+		.table-striped>tbody>tr:nth-child(odd)>td {
+			background-color: honeydew;
+		}
+	</style>
 </head>
 <body>
 
