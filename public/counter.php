e68be9638129574430d29d9a073ba4b273019bb6
Matt Valdez <mattvaldez01@gmail.com>
2017-03-27 13:19:24 -0500

    counter exercise using get request to increment/decrement counter number

---
+++ public/counter.php
@@ -0,0 +1,37 @@
+<?php 
+
+function pageController(){
+	if (!isset($_GET['counter'])) {
+		$_GET['counter'] = 0;
+	}
+	return $_GET;
+}
+extract(pageController());
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Counter</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!--<link rel="stylesheet" href="css/font-awesome.min.css">--> 
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+
+<h1><?=$counter?></h1>
+
+<form method="GET">
+	<a href="/counter.php?counter=<?=$counter+1?>"><button type="button" class="btn-success">Up</button></a>
+	<a href="/counter.php?counter=<?=$counter-1?>"><button type="button" class="btn-danger">Down</button></a>
+</form>
+
+<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
