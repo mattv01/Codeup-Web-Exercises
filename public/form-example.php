5aa25728f1d203556e2a86a9e241b2cdbe05efd7
Matt Valdez <mattvaldez01@gmail.com>
2017-03-28 11:39:27 -0500

    handling user input exercise (working with strip_tags and htmlspecialchars)

---
+++ public/form-example.php
@@ -0,0 +1,28 @@
+<?php
+    $items = array('Item One', 'Item Two', 'Item Three');
+    $allItems = array_merge($items, $_POST);
+
+    function output($item){
+        echo htmlspecialchars(strip_tags($item));
+    }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Alternative Syntax</title>
+</head>
+<body>
+    <h1>List of Items</h1>
+    <ul>
+    <?php foreach ($allItems as $item): ?>
+        <li><?php output($item); ?></li>
+    <?php endforeach; ?>
+    </ul>
+
+    <form method="POST" action="/form-example.php">
+        <input type="text" id="newitem" name="newitem" placeholder="Add new todo item">
+        <input type="submit" value="add">
+    </form>
+</body>
+</html>
