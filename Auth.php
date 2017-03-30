ff2834f1303664b3c4cfd6bb5089b47214720cd5
Matt Valdez <mattvaldez01@gmail.com>
2017-03-30 17:01:39 -0500

    new file with functions to use later in other files

---
+++ Auth.php
@@ -0,0 +1,26 @@
+<?php  
+
+require_once "Log.php";
+
+class Auth {
+
+	public static $password = '$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm';
+	
+	public function attempt($username, $password) {
+	
+	}
+	public function check(){
+	
+	}
+	public function user() {
+	
+	}
+	public function logout(){
+	
+	}
+
+
+}
+
+
+?>
