bae1d5b37a4c78cc2c27de1726732b598e219bbb
Matt Valdez <mattvaldez01@gmail.com>
2017-03-30 20:08:54 -0500

    added instructions to exercise in a comment

--- Auth.php
+++ Auth.php
@@ -9,12 +9,15 @@ class Auth {
 	public function attempt($username, $password) {
 	
 	}
+
 	public function check(){
 	
 	}
+
 	public function user() {
 	
 	}
+	
 	public function logout(){
 	
 	}
@@ -23,4 +26,10 @@ class Auth {
 }
 
 
-?>
+// Auth::attempt() will take in a $username and $password. If the $username is guest and the $password matches the hashed password above then set the LOGGED_IN_USER session variable as before. Use the Log class to log an info message: "User $username logged in.". If either the username or password are incorrect then log an error: "User $username failed to log in!". You will need to use the PHP method password_verify() to check the password hash.
+
+// Auth::check() will return a boolean whether or not a user is logged in.
+
+// Auth::user() will return the username of the currently logged in user.
+
+// Auth::logout() will end the session, just like we did in the sessions exercise.
