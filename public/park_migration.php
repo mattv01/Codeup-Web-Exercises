d160b3f16433c73d8e18682ad812a0690c03fa59
Matt Valdez <mattvaldez01@gmail.com>
2017-04-10 16:31:21 -0500

    this file creates national_parks table

---
+++ public/park_migration.php
@@ -0,0 +1,18 @@
+<?php
+
+require "db_connect.php";
+
+$dropExistingTable = "DROP TABLE IF EXISTS national_parks;";
+$dbc->exec($dropExistingTable);
+
+$createNationalParksTable = "CREATE TABLE national_parks(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	name varchar(100),
+	location varchar(100),
+	date_established DATE,
+	area_in_acres DOUBLE UNSIGNED,
+	PRIMARY KEY (id)
+);";
+$dbc->exec($createNationalParksTable);
+
+?>
