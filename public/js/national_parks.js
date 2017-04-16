e3d6043b33594af91ab7b0ded4d9daa88d69eb21
Matt Valdez <mattvaldez01@gmail.com>
2017-04-15 19:15:23 -0500

    condensed code

--- public/js/national_parks.js
+++ public/js/national_parks.js
@@ -2,24 +2,17 @@
 	"use strict";
 
 	$("td#name").click(function(){
+		$("main").css({"display":"none"});
 		$("body").css("background-image", "url('/img/parks/"+this.innerText+".jpg')");
-		$("main").css({"opacity":"0"});
-		$("h1").html(this.innerText).css("color","white");
-		$("h1").css({"opacity":"0.5"});
-		$("h1").css("background-color","black");
-		$("#table").css({"background-color":"wheat"}); 
-		$("h6").css({"opacity":"1"});
+		$("h1").html(this.innerText).css({"color":"white", "opacity":"0.5", "background-color":"black"});
+		$("h6").css({"display":"block"});
 	});
 
-	$("body").dblclick(function(){
+	$("html").dblclick(function(){
+		$("main").css({"display":"block"});
 		$("body").css("background-image", "none");
-		$("h1").html("National Parks");
-		$("h1").css("opacity","1");
-		$("h1").css("color","black");
-		$("h1").css("background-color","white");
-		$("#table").css({"background-color":"white"});
-		$("main").css({"opacity":"1"});
-		$("h6").css({"opacity":"0"});
+		$("h1").html("National Parks").css({"opacity":"1", "color":"black", "background-color":"white"});
+		$("h6").css({"display":"none"});
 	});
 
 })();
